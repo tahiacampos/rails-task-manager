791029303afd68a555f4aad2c0707aac683313f1
tahia campos <tahia.campos@gmail.com>
2021-07-21 12:23:03 -0400

    add new, show y  for form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,17 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new
+    @task = Task.new
+  end
+
   def show
     @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(params[:task])
+    @task.save
   end
 end
